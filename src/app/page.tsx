808892592541b2caa9d00a2b3191113397e0245d
vineetbs <thevineetbimal@gmail.com>
2025-07-09 16:22:33 +0530

    fix: update mailto link format for email contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -205,7 +205,7 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Contact me at{" "}
                 <a
-                  href={"/mailto:thevineetbimal@gmail.com?"}
+                  href={"mailto:thevineetbimal@gmail.com?"}
                   className="underline text-blue-800"
                 >
                   thevineetbimal@gmail.com
